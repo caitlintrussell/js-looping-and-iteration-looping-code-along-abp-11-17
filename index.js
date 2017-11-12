598396ed04daee0f717aebe57dbd4d5080f6f8b8
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-12 18:26:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const names = ['Ada', 'Brendan', 'Ali'];
 
 function printBadges(names) {
   for (let x = 0; x < names.length; x++){
-    console.log(`Welcome ${names[x]}! You are employee #${x + 1}.`)
+    console.log(`Welcome ${names[x]}! You are employee #${x+1}.`)
   }
   return names;
 }
 printBadges(names);
 
 
-function tailsNeverFails(){
- return Math.random() ;
+function coinToss(){
+ return Math.random() >= 0.5;
 }
-let counter = 1;
-while (Math.random() >= 0.5){
+let counter = 0;
+while (coinToss()){
   ++counter;
+}
   console.log(`You got ${counter} tails in a row!`);
 }
