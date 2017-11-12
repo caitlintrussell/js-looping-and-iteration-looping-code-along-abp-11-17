04ab6bad9a1b8bcf592ba5eeb6c3c09778535f1c
Caitlin Trussell <caitlintrussell@gmail.com>
2017-11-12 18:22:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const names = ['Ada', 'Brendan', 'Ali'];
+
+function printBadges(names) {
+  for (let x = 0; x < names.length; x++){
+    console.log(`Welcome ${names[x]}! You are employee #${x + 1}.`)
+  }
+  return names;
+}
+printBadges(names);
+
+
+function tailsNeverFails(){
+ return Math.random() ;
+}
+let counter = 1;
+while (Math.random() >= 0.5){
+  ++counter;
+  console.log(`You got ${counter} tails in a row!`);
+}
